6be6eab61cae52d869ff723c4a698a3277743b0f
Rodrigo <rodrigoarcanjo690@gmail.com>
2024-08-13 19:18:51 -0300

    update controller

---
+++ desafio-todolist/src/main/java/br/com/rodrigoarcanjo/desafiotodolist/controller/TodoController.java
@@ -0,0 +1,48 @@
+package br.com.rodrigoarcanjo.desafiotodolist.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rodrigoarcanjo.desafiotodolist.entity.Todo;
+import br.com.rodrigoarcanjo.desafiotodolist.service.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
